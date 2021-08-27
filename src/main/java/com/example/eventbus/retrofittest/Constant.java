77fda0b74b821564fe5d929340afabf1e39f3c29
wyl <76277193+wyljyllz@users.noreply.github.com>
2021-08-27 15:28:16 +0800

    Delete src directory

--- src/main/java/com/example/eventbus/retrofittest/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.example.eventbus.retrofittest;
-
-public class Constant {
-    //baseurl
-    public final static String URL_BASE = "http://localhost:8081/zys/iot/";
-}
--- src/main/java/com/example/eventbus/view/MainView.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventbus.view;
-
-import com.example.dto.MyResponse;
-
-public interface MainView extends BaseView {
-    void onTextSuccess(MyResponse o);
-}
